ca9e391d6cd2e51ed8dc770631490c8b532082fe
Pete Holdsworth <Pete@petes-air.home>
2016-08-28 22:36:01 +0100

    Project set up

--- AlbmsApp/ViewController.swift
+++ AlbmsApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import pop
 
 class ViewController: UIViewController {
 
@@ -15,10 +16,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
